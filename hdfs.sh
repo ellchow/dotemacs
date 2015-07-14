e24a31988b65f7380ff8779f590cba74927dd82e
Elliot Chow <chow.elliot@gmail.com>
2015-07-14 12:09:47 -0700

    update

--- hdfs.sh
+++ hdfs.sh
@@ -9,4 +9,22 @@ function hmv() { hadoop fs -mv "$@" ; }
 function hcp() { hadoop fs -cp "$@" ; }
 function htxt() { hadoop fs -text "$@" ; }
 function hput() { hadoop fs -put "$@" ; }
-function hdus() { hadoop fs -du -s "$@" }
+function hdus() { hadoop fs -du -s "$@" | awk '
+BEGIN {
+  u[0] = "B"
+  u[1] = "K"
+  u[2] = "M"
+  u[3] = "G"
+  u[4] = "T"
+}
+{
+    n = length($1)
+    p = int(n / 3)
+    p = (p > 4 ? 4 : p)
+    x = $1
+    for (i = 0; i < p; i += 1) {
+       x = x / 1000
+    }
+    print $1 , x "" u[p] , $2
+}
+' ; }
