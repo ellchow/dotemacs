71c0fa6eca87c16836ec9aa1e33ba9c490aab7dd
Elliot Chow <chow.elliot@gmail.com>
2015-10-28 16:57:33 -0700

    some brews

--- setup-mac.sh
+++ setup-mac.sh
@@ -3,9 +3,14 @@
 ## brew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+
+
+brew install watch
+brew install tmux
+brew install gawk
+brew install coreutils
+brew install the_silver_searcher
+
+
 # pip install glances
-
-# brew install watch
-
-# brew install tmux
 # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
