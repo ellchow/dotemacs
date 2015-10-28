c5e1badaa7144a8091018d50199f6278f7eb4422
Elliot Chow <chow.elliot@gmail.com>
2015-10-28 16:59:26 -0700

    user name and email

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ wd=`dirname $0`
 
 cd $wd
 
+git config user.name "Elliot Chow"
+git config user.email chow.elliot@gmail.com
+
 files='.emacs elisp .bashrc .bash_profile .git-completion.sh .pystartup .Rprofile .screenrc .gitconfig .tmux.conf'
 
 for x in $files; do
