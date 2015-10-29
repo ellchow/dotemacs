43cd00120d05d8903d986ddf7efd8309077fa7ed
Elliot Chow <chow.elliot@gmail.com>
2015-10-28 17:00:27 -0700

    tmux plugins

--- setup-mac.sh
+++ setup-mac.sh
@@ -7,10 +7,10 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 brew install watch
 brew install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 brew install gawk
 brew install coreutils
 brew install the_silver_searcher
 
 
 # pip install glances
-# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
