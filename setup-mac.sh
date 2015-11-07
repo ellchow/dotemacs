2cc9408420831d2eb28939416e1b3419236e3683
Elliot Chow <chow.elliot@gmail.com>
2015-11-06 16:55:48 -0800

    some notes

--- setup-mac.sh
+++ setup-mac.sh
@@ -3,13 +3,27 @@
 ## brew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-
-
+brew install emacs
 brew install watch
-brew install tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 brew install gawk
 brew install coreutils
 brew install the_silver_searcher
+brew install rename
 
-pip install glances
+brew tap caskroom/cask
+brew install brew-cask
+brew cask install java
+
+brew install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# sudo easy_install pip && pip install glances
+
+# brew install webfs
+
+# brew install sbt
+
+# brew install docker
+# brew install docker-machine
+# brew cask install virtualbox
+# docker-machine create --driver virtualbox default
